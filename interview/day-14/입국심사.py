95a53d85aec09ef59578d9269b437e4ac72371c5
geonwoo <yung65322136@gmail.com>
2022-05-30 09:08:51 +0900

    .
    .

---
+++ interview/day-14/입국심사.py
@@ -0,0 +1,30 @@
+def solution(n, times):
+    answer = 0
+    l, r = 1, max(times) * n
+    while l > r:
+        mid = (left + right) // 2
+        people = 0
+        for time in times:
+            # people 은 모든 심사관들이 mid분 동안 심사한 사람의 수
+            people += mid // time
+            # 모든 심사관을 거치지 않아도 mid분 동안 n명 이상의 심사를 할 수 있다면 반복문을 나간다.
+            if people >= n:
+                break
+
+        # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 많거나 같은 경우
+        if people >= n:
+            answer = mid
+            right = mid - 1
+        # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 적은 경우
+        elif people < n:
+            left = mid + 1
+
+    return answer
+print(solution(6, [7, 10]))
+
+# 7 1
+# 10 1
+
+# 4
+#
+# 6	[7, 10]	28
--- interview/day-14/회전 정렬 배열에서 검색.py
+++ interview/day-14/회전 정렬 배열에서 검색.py
@@ -6,8 +6,9 @@ def search(nums: List[int], target: int) -> int:
     min_idx = sorted([[i,v] for i,v in enumerate(nums)], key=lambda x:x[1])[0][0]
     nums = nums[min_idx:] + nums[:min_idx]
     i = bisect.bisect_left(nums, target)
+    print(i)
     if len(nums) > i and nums[i] == target:
         return (i+min_idx) % len(nums)
     else: return -1
 
-print(search([4, 5, 6, 7, 0, 1, 2], 0))
+print(search([5, 6, 0, 1, 2, 3, 4], 1))
