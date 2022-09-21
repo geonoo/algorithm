dfc06ef90f330a8f776059438e795b11f66f9d1f
geonoo <yung65322136@gmail.com>
2022-09-21 15:35:08 +0900

    #1 프로그래머스
    ㅎㅎ

---
+++ programmers/성격 유형 검사.py
@@ -0,0 +1,16 @@
+def solution(survey, choices):
+    
+    answer = ''
+    return answer
+
+
+
+print(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]))
+
+# ["AN", "CF", "MJ", "RT", "NA"]	[5, 3, 2, 7, 5]	"TCMA"
+# ["TR", "RT", "TR"]	[7, 1, 3]	"RCJA"
+
+# 1번 지표	라이언형(R)	0	튜브형(T)	0
+# 2번 지표	콘형(C)	0	프로도형(F)	0
+# 3번 지표	제이지형(J)	0	무지형(M)	0
+# 4번 지표	어피치형(A)	0	네오형(N)	0
